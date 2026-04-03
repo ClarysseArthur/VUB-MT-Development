f6d0e6d610dd4b8aed9a7a76f97b485f07d6307e
Arthur Clarysse <info@claryssearthur.be>
2026-04-03 15:58:32 +0200

    Refactored Gradient-Based Attacks

--- Attack/FGSM.py
+++ Attack/FGSM.py
@@ -1,247 +1,115 @@
 import torch
 
 
-class FGSM_1V:
-    def __init__(self, net_in, net_out, kPCA, opt):
-        self.net_in = net_in
-        self.net_out = net_out
-        self.kPCA = kPCA
-        self.opt = opt
-
-    def run_model_batch_1V(self, img_batch):
-        x_en = self.net_in(img_batch)
-
-        h, _ = self.kPCA(x_en)
-        U = torch.mm(x_en.t(), h)
-
-        x_tilde = self.net_out(torch.mm(h, U.t()))
-        return x_tilde
-
-    def create_adversarial_batch(self, in_images, epsilon=0.1):
-        self.net_in.zero_grad()
-        self.net_out.zero_grad()
-
-        adv_images = in_images.clone().detach().to(self.opt.device)
-        adv_images.requires_grad_(True)
-
-        out_images = self.run_model_batch_1V(adv_images)
-
-        loss = torch.nn.functional.mse_loss(out_images, in_images)
-        loss.backward()
-
-        signed_grad = adv_images.grad.sign()
-        adv_images = adv_images + epsilon * signed_grad
-        adv_images = torch.clamp(adv_images, 0, 1)
-
-        return adv_images.detach(), signed_grad.detach()
-
-    def create_adversarial_batch_bim(self, in_images, epsilon=0.1, alpha=0.01, num_iterations=10):
-        adv_images = in_images.clone().detach().to(self.opt.device)
-
-        image_plus = in_images + epsilon
-        image_minus = in_images - epsilon
-
-        for _ in range(num_iterations):
-            self.net_in1.zero_grad()
-            self.net_out1.zero_grad()
-
-            adv_images = adv_images.clone().detach()
-            adv_images.requires_grad_(True)
-
-            out_images, _ = self.run_model_batch(adv_images)
-
-            loss = torch.nn.functional.mse_loss(out_images, in_images)
-            loss.backward()
-
-            signed_grad = adv_images.grad.sign()
-
-            # Iterative FGSM step
-            image_prime = adv_images + alpha * signed_grad
-
-            # Clip to epsilon-ball around original image, then to valid image range
-            adv_images = torch.max(image_minus, image_prime)
-            adv_images = torch.min(image_plus, adv_images)
-            adv_images = torch.clamp(adv_images, 0, 1)
-
-        return adv_images.detach(), signed_grad.detach()
-
-
-class FGSM_2V:
-    def __init__(self, net_in1, net_in2, net_out1, net_out2, kPCA, opt):
-        self.net_in1 = net_in1
-        self.net_in2 = net_in2
-        self.net_out1 = net_out1
-        self.net_out2 = net_out2
-        self.kPCA = kPCA
-        self.opt = opt
-
-    def run_model_batch(self, img_batch, label_batch):
-        x_en = self.net_in1(img_batch)
-        y_en = self.net_in2(label_batch)
-
-        h, _ = self.kPCA(x_en, y_en)
-        U = torch.mm(x_en.t(), h)
-        V = torch.mm(y_en.t(), h)
-
-        x_tilde = self.net_out1(torch.mm(h, U.t()))
-        y_tilde = self.net_out2(torch.mm(h, V.t()))
-        return x_tilde, y_tilde
-
-    def create_adversarial_batch(self, in_images, in_labels, epsilon=0.1):
-        self.net_in1.zero_grad()
-        self.net_in2.zero_grad()
-        self.net_out1.zero_grad()
-        self.net_out2.zero_grad()
-
-        adv_images = in_images.clone().detach().to(self.opt.device)
-        adv_images.requires_grad_(True)
-        in_labels = in_labels.to(self.opt.device)
-
-        out_images, _ = self.run_model_batch(adv_images, in_labels) # 1. Forward clean image batch through the model
-
-        loss = torch.nn.functional.mse_loss(out_images, in_images)  # 2. Compute reconstruction loss between output and original input
-        loss.backward()                                             # 3. Backpropagate to compute gradients w.r.t. input images
-
-        signed_grad = adv_images.grad.sign()                        # 4.a. Take the sign of the gradients to get the direction of maximum increase in loss
-        adv_images = adv_images + epsilon * signed_grad             # 4.b. Create adversarial images by adding a small perturbation (𝜀) in the direction of the gradient
-        adv_images = torch.clamp(adv_images, 0, 1)                  # 4.c. Ensure the adversarial images are valid pixel values (between 0 and 1)
-
-        return adv_images.detach(), signed_grad.detach()
-
-    def create_adversarial_batch_bim(self, in_images, in_labels, epsilon=0.1, alpha=0.01, num_iterations=10):
-        adv_images = in_images.clone().detach().to(self.opt.device)
-        in_labels = in_labels.clone().detach().to(self.opt.device)
-
-        image_plus = in_images + epsilon
-        image_minus = in_images - epsilon
-
-        for _ in range(num_iterations):
-            self.net_in1.zero_grad()
-            self.net_in2.zero_grad()
-            self.net_out1.zero_grad()
-            self.net_out2.zero_grad()
-
-            adv_images = adv_images.clone().detach()
-            adv_images.requires_grad_(True)
-
-            out_images, _ = self.run_model_batch(adv_images, in_labels)
-
-            loss = torch.nn.functional.mse_loss(out_images, in_images)
-            loss.backward()
-
-            signed_grad = adv_images.grad.sign()
-
-            # Iterative FGSM step
-            image_prime = adv_images + alpha * signed_grad
-
-            # Clip to epsilon-ball around original image, then to valid image range
-            adv_images = torch.max(image_minus, image_prime)
-            adv_images = torch.min(image_plus, adv_images)
-            adv_images = torch.clamp(adv_images, 0, 1)
-
-        return adv_images.detach(), signed_grad.detach()
-        
-
-class FGSM_3V:
-    def __init__(self, net_in1, net_in2, net_in3, net_out1, net_out2, net_out3, kPCA, partial_kPCA, opt):
+class FGSM:
+    def __init__(self, net_in1, net_out1, net_in2=None, net_out2=None, net_in3=None, net_out3=None, kPCA=None, opt=None):
         self.net_in1 = net_in1
         self.net_in2 = net_in2
         self.net_in3 = net_in3
+
         self.net_out1 = net_out1
         self.net_out2 = net_out2
         self.net_out3 = net_out3
+
         self.kPCA = kPCA
-        self.partial_kPCA = partial_kPCA
         self.opt = opt
 
-    def run_model_batch(self, img_batch, sketch_batch, label_batch):
-        x_en = self.net_in1(img_batch)
-        y_en = self.net_in2(sketch_batch)
-        z_en = self.net_in3(label_batch)
-
-        h, _ = self.kPCA(x_en, y_en, z_en)
-        U = torch.mm(x_en.t(), h)
-        V = torch.mm(y_en.t(), h)
-        W = torch.mm(z_en.t(), h)
-
-        x_tilde = self.net_out1(torch.mm(h, U.t()))
-        y_tilde = self.net_out2(torch.mm(h, V.t()))
-        z_tilde = self.net_out3(torch.mm(h, W.t()))
-        return x_tilde, y_tilde, z_tilde
-
-    def run_model_batch_noimview(self, sketch_batch, label_batch):
-        # x_en = self.net_in1(image_batch)
-        y_en = self.net_in2(sketch_batch)
-        z_en = self.net_in3(label_batch)
-
-        h, _ = self.partial_kPCA([y_en, z_en]) # Image view not used for the latent representation
-
-        U = torch.mm(h, h)
-        V = torch.mm(y_en.t(), h)
-        W = torch.mm(z_en.t(), h)
-
-        x_tilde = self.net_out1(torch.mm(h, U.t()))
-        y_tilde = self.net_out2(torch.mm(h, V.t()))
-        z_tilde = self.net_out3(torch.mm(h, W.t()))
-
-        return x_tilde, y_tilde, z_tilde
-
-    def create_adversarial_batch(self, in_images, in_sketch, in_labels, epsilon=0.1):
         self.net_in1.zero_grad()
-        self.net_in2.zero_grad()
-        self.net_in3.zero_grad()
+        self.net_in2.zero_grad() if self.net_in2 is not None else None
+        self.net_in3.zero_grad() if self.net_in3 is not None else None
         self.net_out1.zero_grad()
-        self.net_out2.zero_grad()
-        self.net_out3.zero_grad()
+        self.net_out2.zero_grad() if self.net_out2 is not None else None
+        self.net_out3.zero_grad() if self.net_out3 is not None else None
 
-        adv_images = in_images.clone().detach().to(self.opt.device)
-        adv_images.requires_grad_(True)
-        in_labels = in_labels.to(self.opt.device)
-        in_sketch = in_sketch.to(self.opt.device)
+    def _kPCA(self, x_en, y_en=None, z_en=None):
+        if y_en is not None and z_en is not None:
+            return self.kPCA(x_en, y_en, z_en)
+        elif y_en is not None:
+            return self.kPCA(x_en, y_en)
+        else:
+            return self.kPCA(x_en)
 
-        out_images, _, _ = self.run_model_batch(adv_images, in_sketch, in_labels)
+    def run_model_batch(self, b1, b2=None, b3=None):
+        x_en = self.net_in1(b1)
+        y_en = self.net_in2(b2) if b2 is not None else None
+        z_en = self.net_in3(b3) if b3 is not None else None
 
-        loss = torch.nn.functional.mse_loss(out_images, in_images)
-        loss.backward()
+        h, _ = self._kPCA(x_en, y_en, z_en)
+        U = torch.mm(x_en.t(), h)
+        V = torch.mm(y_en.t(), h) if y_en is not None else None
+        W = torch.mm(z_en.t(), h) if z_en is not None else None
 
-        signed_grad = adv_images.grad.sign()
-        adv_images = adv_images + epsilon * signed_grad
-        adv_images = torch.clamp(adv_images, 0, 1)
+        x_tilde = self.net_out1(torch.mm(h, U.t()))
+        y_tilde = self.net_out2(torch.mm(h, V.t())) if V is not None else None
+        z_tilde = self.net_out3(torch.mm(h, W.t())) if W is not None else None
+        return x_tilde, y_tilde, z_tilde
 
-        return adv_images.detach(), signed_grad.detach()
+    def create_adversarial_batch_fgsm(self, in1, in2=None, in3=None, epsilon=0.1):
+        x_adv = in1.clone().detach().to(self.opt.device)
+        x_adv.requires_grad_(True)
 
-    def create_adversarial_batch_bim(self, in_images, in_sketch, in_labels, epsilon=0.1, alpha=0.01, num_iterations=10):
-        adv_images = in_images.clone().detach().to(self.opt.device)
-        in_labels = in_labels.clone().detach().to(self.opt.device)
-        in_sketch = in_sketch.clone().detach().to(self.opt.device)
+        y = in2.to(self.opt.device) if in2 is not None else None
+        z = in3.to(self.opt.device) if in3 is not None else None
 
-        image_plus = in_images + epsilon
-        image_minus = in_images - epsilon
+        out_x, _, _ = self.run_model_batch(x_adv, y, z)                   # 1. Forward clean image batch through the model
+
+        loss = torch.nn.functional.mse_loss(out_x, in1)             # 2. Compute reconstruction loss between output and original input
+        loss.backward()                                             # 3. Backpropagate to compute gradients w.r.t. input images
+
+        signed_grad = x_adv.grad.sign()                             # 4.a. Take the sign of the gradients to get the direction of maximum increase in loss
+        x_adv = x_adv + epsilon * signed_grad                       #   b. Create adversarial images by adding a small perturbation (𝜀) in the direction of the gradient
+        x_adv = torch.clamp(x_adv, 0, 1)                            #   c. Ensure the adversarial images are valid pixel values (between 0 and 1)
+
+        return x_adv.detach(), signed_grad.detach()
+
+    def create_adversarial_batch_bim(self, in1, in2=None, in3=None, epsilon=0.1, alpha=0.01, num_iterations=10):
+        adv_x = in1.clone().detach().to(self.opt.device)
+        y = in2.to(self.opt.device) if in2 is not None else None
+        z = in3.to(self.opt.device) if in3 is not None else None
+
+        image_plus = in1 + epsilon
+        image_minus = in1 - epsilon
 
         for _ in range(num_iterations):
-            self.net_in1.zero_grad()
-            self.net_in2.zero_grad()
-            self.net_in3.zero_grad()
-            self.net_out1.zero_grad()
-            self.net_out2.zero_grad()
-            self.net_out3.zero_grad()
+            adv_x = adv_x.clone().detach()
+            adv_x.requires_grad_(True)
 
-            adv_images = adv_images.clone().detach()
-            adv_images.requires_grad_(True)
+            out_x, _, _ = self.run_model_batch(adv_x, y, z)
 
-            out_images, _, _ = self.run_model_batch(adv_images, in_sketch, in_labels)
-
-            loss = torch.nn.functional.mse_loss(out_images, in_images)
+            loss = torch.nn.functional.mse_loss(out_x, in1)
             loss.backward()
 
-            signed_grad = adv_images.grad.sign()
+            signed_grad = adv_x.grad.sign()
 
             # Iterative FGSM step
-            image_prime = adv_images + alpha * signed_grad
+            image_prime = adv_x + alpha * signed_grad
 
             # Clip to epsilon-ball around original image, then to valid image range
-            adv_images = torch.max(image_minus, image_prime)
-            adv_images = torch.min(image_plus, adv_images)
-            adv_images = torch.clamp(adv_images, 0, 1)
+            adv_x = torch.max(image_minus, image_prime)
+            adv_x = torch.min(image_plus, adv_x)
+            adv_x = torch.clamp(adv_x, 0, 1)
 
-        return adv_images.detach(), signed_grad.detach()
+        return adv_x.detach(), signed_grad.detach()
+
+    def create_adversarial_batch_lbfgs(self, in1, in2=None, in3=None, epsilon=0.1, num_steps=50):
+        adv_x = in1.clone().detach().to(self.opt.device)
+        adv_x.requires_grad_(True)
+        y = in2.to(self.opt.device) if in2 is not None else None
+        z = in3.to(self.opt.device) if in3 is not None else None
+
+        optimizer = torch.optim.LBFGS([adv_x], max_iter=num_steps)
+
+        def closure():
+            optimizer.zero_grad()
+            out_x, _, _ = self.run_model_batch(adv_x, y, z)
+            loss = -torch.nn.functional.mse_loss(out_x, in1)
+            loss.backward()
+            return loss
+
+        optimizer.step(closure)
+
+        # Ensure the adversarial images are within the epsilon-ball and valid pixel range
+        adv_x = torch.clamp(adv_x, in1 - epsilon, in1 + epsilon)
+        adv_x = torch.clamp(adv_x, 0, 1)
+
+        return adv_x.detach(), None
